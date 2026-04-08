7f2a6435efe4760a0d31a0d4c2c6d146398f268c
Andres06B <afbh0006@gmail.com>
2026-04-07 20:25:49 -0500

    iniciando repositorio

---
+++ src/app/features/home/components/coverage/coverage.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-coverage',
+  imports: [],
+  templateUrl: './coverage.html',
+  styleUrl: './coverage.css',
+})
+export class Coverage {}
