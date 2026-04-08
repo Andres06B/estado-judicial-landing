24c4c82a9fcd18423182fd0b1d1045e2abef7dc3
Andres06B <afbh0006@gmail.com>
2026-04-08 15:51:16 -0500

    Añadiendo header y hero

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import path from 'path';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', loadComponent: () => import('./layout/main-layout/main-layout').then(m => m.MainLayout),
+        children: [
+            {
+                path: '',loadComponent: () => import('./features/home/home').then(m => m.Home)
+            }
+        ]
+    }
+];
---
+++ src/app/layout/simple-layout/simple-layout.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-simple-layout',
+  imports: [],
+  templateUrl: './simple-layout.html',
+  styleUrl: './simple-layout.css',
+})
+export class SimpleLayout {}
